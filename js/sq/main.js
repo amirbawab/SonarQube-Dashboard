093a26ecd37186b0ceb4c5537ab5f875aef976d6
Amir Bawab <amirbawab@gmail.com>
2016-09-05 21:01:45 -0400

    Added metrics

--- js/sq/main.js
+++ js/sq/main.js
@@ -1,5 +1,14 @@
 
-
+// Set datepicker fields
 $('.datepicker').datepicker({
     format: 'mm/dd/yyyy'
+});
+
+// Set multiselect fields
+$('#metrics').multiselect({
+     enableClickableOptGroups: true,
+     enableCaseInsensitiveFiltering: true,
+     enableFiltering: true,
+     buttonWidth: '100%',
+     enableCollapsibleOptGroups: true
 });
